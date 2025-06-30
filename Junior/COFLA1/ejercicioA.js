9d441f66fb8362f76688c1e8f7e77624f54c5019
Ema <emanuelgarciarios@gmail.com>
2025-06-30 01:35:36 -0500

    Agregando la posibilidad de saber cuanto  es el vuelto>

--- Junior/COFLA1/ejercicioA.js
+++ Junior/COFLA1/ejercicioA.js
@@ -1,11 +1,11 @@
 const definirCompra = (n) =>{
-    let dinero = prompt(`Ingresa el dinero de ${n}`);
-    if (dinero >= 0.6 && dinero <1)return (`${n}: Helado de agua`);
-    if (dinero >= 1 && dinero <1.6)return (`${n}: Helado de crema`);
-    if (dinero >= 1.6 && dinero <1.7)return (`${n}: Helado de heladix`);
-    if (dinero >= 1.7 && dinero <1.8)return (`${n}: Helado de heladovich`);
-    if (dinero >= 1.8 && dinero <2.9)return (`${n}: Helado de helardo`);
-    if (dinero >= 2.9)return (`${n}: Potecito de helado con confites o pote de 1/4 kg`);
+    let dinero = parseFloat(prompt(`Ingresa el dinero de ${n}`));
+    if (dinero >= 0.6 && dinero <1)return (`${n}: Helado de agua y el vuelto es:${(dinero-0.6).toFixed(2)}`);
+    if (dinero >= 1 && dinero <1.6)return (`${n}: Helado de crema y el vuelto es:${(dinero-1).toFixed(2)}`);
+    if (dinero >= 1.6 && dinero <1.7)return (`${n}: Helado de heladix y el vuelto es:${(dinero-1.6).toFixed(2)}`);
+    if (dinero >= 1.7 && dinero <1.8)return (`${n}: Helado de heladovich y el vuelto es:${(dinero-1.7).toFixed(2)}`);
+    if (dinero >= 1.8 && dinero <2.9)return (`${n}: Helado de helardo y el vuelto es:${(dinero-1.8).toFixed(2)}`);
+    if (dinero >= 2.9)return (`${n}: Potecito de helado con confites o pote de 1/4 kg y el vuelto es:${(dinero-2.9).toFixed(2)}`);
     else return (`${n}: No te alcanza para nada`);
 }
 
